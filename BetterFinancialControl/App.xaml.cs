b68c3b48e415ef479a83bd74730f546c0cb7b47f
Paulo Paes <paulo.paes@wises.com.br>
2024-05-20 20:18:56 -0300

    Adicionar arquivos de projeto.

---
+++ BetterFinancialControl/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BetterFinancialControl
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
