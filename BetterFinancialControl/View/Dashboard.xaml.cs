607ee33f56263f089c9dabe640d9ea18aff35c9e
Paulo Paes <paulo.paes@wises.com.br>
2024-06-01 19:45:02 -0300

    adiciona lista no dashboard

--- BetterFinancialControl/View/Dashboard.xaml.cs
+++ BetterFinancialControl/View/Dashboard.xaml.cs
@@ -1,3 +1,5 @@
+using BetterFinancialControl.Repository;
+
 namespace BetterFinancialControl.View;
 
 public partial class Dashboard : ContentPage
@@ -5,5 +7,8 @@ public partial class Dashboard : ContentPage
 	public Dashboard()
 	{
 		InitializeComponent();
+/*		var repository = new DashboardRepository();
+		var dashboard = repository.Buscar(DateTime.Now, 1);
+		dashboardView.ItemsSource = dashboard.Movimentacoes;*/
 	}
 }
