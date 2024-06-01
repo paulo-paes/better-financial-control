eaf414ba45c6f3edab320ff1ac56f6e46f054cb0
Pedro Maschio <pedrohenriquemaschio@gmail.com>
2024-06-01 14:20:06 -0300

    criação da tela de login

--- BetterFinancialControl/App.xaml.cs
+++ BetterFinancialControl/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace BetterFinancialControl
+﻿using BetterFinancialControl.View;
+namespace BetterFinancialControl
 {
     public partial class App : Application
     {
@@ -6,7 +7,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Login(); ;
         }
     }
 }
---
+++ BetterFinancialControl/View/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace BetterFinancialControl.View;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
