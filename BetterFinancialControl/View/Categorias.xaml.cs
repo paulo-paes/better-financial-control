8b4cd9352daafe2240a18a0adc21e549a21183d4
Pedro Maschio <pedrohenriquemaschio@gmail.com>
2024-06-09 16:12:17 -0300

    correção login

--- BetterFinancialControl/View/Categorias.xaml.cs
+++ BetterFinancialControl/View/Categorias.xaml.cs
@@ -1,9 +1,20 @@
+using BetterFinancialControl.Model;
+using BetterFinancialControl.Repository;
+using System.ComponentModel;
+
 namespace BetterFinancialControl.View;
 
 public partial class Categorias : ContentPage
 {
+	public CategoriaRepository categoriaRep;
 	public Categorias()
 	{
 		InitializeComponent();
+	}
+	public void listarCategorias()
+	{
+		List<Categoria> listaCategorias = new List<Categoria>();
+		
+	
 	}
 }
