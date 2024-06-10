cbcd3ab92fef5ecfb6d4f7272689a60c4e432741
Pedro Maschio <pedrohenriquemaschio@gmail.com>
2024-06-09 21:13:03 -0300

    categoria

--- BetterFinancialControl/View/Categorias.xaml.cs
+++ BetterFinancialControl/View/Categorias.xaml.cs
@@ -6,15 +6,56 @@ namespace BetterFinancialControl.View;
 
 public partial class Categorias : ContentPage
 {
-	public CategoriaRepository categoriaRep;
 	public Categorias()
 	{
-		InitializeComponent();
-	}
-	public void listarCategorias()
-	{
-		List<Categoria> listaCategorias = new List<Categoria>();
-		
+        InitializeComponent();
+      
+    }
+
+
 	
-	}
+
+    private async void adicionarCategoriaBtn_Clicked(object sender, EventArgs e)
+    {
+        CategoriaRepository categoriaRepository = new CategoriaRepository();
+        MenuPage menuPage = App.Current?.MainPage as MenuPage;
+
+        string categoriaDescricao;
+
+        categoriaDescricao = await DisplayPromptAsync("Nova categoria", "Preencha a descrição da nova categoia:", initialValue: "", keyboard: Keyboard.Default);
+      
+        if (categoriaDescricao != null)
+        {
+            var novaCategoria = new Categoria();
+            novaCategoria.UserId = menuPage.usuarioAtual.Id;
+            novaCategoria.Nome = categoriaDescricao;
+
+            try
+            { 
+                categoriaRepository.Criar(novaCategoria, menuPage.usuarioAtual.Id);
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("ERRO", ex.Message, "OK");
+            }
+        }
+        listarCategorias();
+    }
+    
+    public void listarCategorias()
+    {
+        CategoriaRepository categoriaRepository = new CategoriaRepository();
+        MenuPage menuPage = App.Current?.MainPage as MenuPage;
+        List<Categoria> categoriasCadastradas = new List<Categoria>();
+        foreach(Categoria i in categoriaRepository.Buscar(menuPage.usuarioAtual.Id))
+        {
+            categoriasCadastradas.Add(i);
+;       }
+        categoriaListView.ItemsSource = categoriasCadastradas;
+    }
+
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        listarCategorias();
+    }
 }
